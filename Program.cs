9fc63ad4080e10aeb43d55127630796c7bd366f2
guilherme domingos <guilherme.santos240503@gmail.com>
2025-03-23 17:11:21 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+   Console.Clear();
+
+        Console.WriteLine("O que é, o que é? o que pesa mais no mundo??");
+     
+
+        Thread.Sleep(5000);
+
+       
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(" A Thais Carla!");
+        Console.ResetColor();
+ 
+
+       
